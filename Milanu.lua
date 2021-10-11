10eff54a54569fa469f0e05e444152a85d664b45
MilanuTEAM <90847166+MilanuTEAM@users.noreply.github.com>
2021-10-11 12:01:26 +0300

    Add files via upload

--- Milanu.lua
+++ Milanu.lua
@@ -25,7 +25,7 @@ if io.open("File_Bot/commands.lua","r") then
 io.close(io.open("File_Bot/commands.lua","r"))
 print("done => commands.lua")
 else
-io.popen("cd File_Bot && wget https://raw.githubusercontent.com/MilanuTEAM/Files_Milanu/main/File_Bot/commands.lua") 
+io.popen("cd File_Bot && wget https://raw.githubusercontent.com/MilanuTEAM/MilanuFiles/main/File_Bot/commands.lua") 
 print("end wget => commands.lua")
 end
 t = "\27[35m".."\nAll Files Started : \n____________________\n"..'\27[m'
@@ -3780,7 +3780,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if DevMilanuW(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/MilanuTEAM/Files_Milanu/main/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/MilanuTEAM/MilanuFiles/main/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -3818,7 +3818,7 @@ t = " *⌯︙الملف »* "..file.."\n*⌯︙تم تعطيل ملف* \n"
 else
 t = " *⌯︙بالتاكيد تم تعطيل ملف →* "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/MilanuTEAM/Files_Milanu/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/MilanuTEAM/MilanuFiles/main/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -3838,7 +3838,7 @@ t = " *⌯︙بالتاكيد تم تفعيل ملف →* "..file.." \n"
 else
 t = " *⌯︙الملف »* "..file.."\n*⌯︙تم تفعيل ملف* \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/MilanuTEAM/Files_Milanu/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/MilanuTEAM/MilanuFiles/main/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
